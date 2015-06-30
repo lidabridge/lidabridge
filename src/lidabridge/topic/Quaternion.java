9618efeb645b6f0f34b6ce6f8e76a758e5800e26
Thiago Becker <tbecker@TBECKERLX.ITAIPU>
2015-06-30 16:35:03 -0300

    Added Quaternion for PoseStampedTopic

---
+++ src/lidabridge/topic/Quaternion.java
@@ -0,0 +1,42 @@
+package lidabridge.topic;
+
+public class Quaternion {
+
+  private float x;
+  private float y;
+  private float z;
+  private float w;
+
+	public float getX() {
+    return x;
+  }
+
+  public float setX(x) {
+    this.x = x;
+  }
+
+  public float getY() {
+    return y;
+  }
+
+  public float setY(y) {
+    this.y = y;
+  }
+
+  public float getZ() {
+    return z;
+  }
+
+  public float setZ(z) {
+    this.z = z;
+  }
+
+  public float getW() {
+    return w;
+  }
+
+  public float setW(w) {
+    this.w = w;
+  }
+
+}
